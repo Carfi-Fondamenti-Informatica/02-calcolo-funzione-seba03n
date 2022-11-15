13a394df387b2245f2e84d08b0cbf6fa93020ac3
seba03n <115528575+seba03n@users.noreply.github.com>
2022-11-15 17:11:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ int main() {
    float a,b,x,y,z;
     
     if(x<0 & y>0){
-        cout<<a*x-b*y<<endl;
+        cout<<z = a*x-b*y<<endl;
     }
     else if(x>=0 & x<=0){
-        cout<<a*(x*x)-b*y<<endl;
+        cout<<z = a*(x*x)-b*y<<endl;
     }
     else{
-        cout<<a*y+b*(y*y);
+        cout<<z = a*y+b*(y*y);
     }
    
    return 0;
