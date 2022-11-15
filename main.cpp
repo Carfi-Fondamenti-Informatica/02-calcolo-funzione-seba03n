6a04fabd1c03ba2c88e5491cac260196dec3948c
seba03n <115528575+seba03n@users.noreply.github.com>
2022-11-15 17:09:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   
+   float a,b,x,y,z;
+    
+    if(x<0 & y>0){
+        cout<<a*x-b*y<<endl;
+    }
+    else if(x>=0 & x<=0){
+        cout<<a*(x*x)-b*y<<endl;
+    }
+    else{
+        cout<<a*y+b*(y*y);
+    }
+   
    return 0;
 }
